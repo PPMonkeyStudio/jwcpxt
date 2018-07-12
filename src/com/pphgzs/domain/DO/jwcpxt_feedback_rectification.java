18a7843cd89d95326ad844c2afa4eb6e9f69a0c3
ZB <www.1037976812@qq.com>
2018-07-12 11:49:20 +0800

    没有，快滚，再问老子打死你

--- src/com/pphgzs/domain/DO/jwcpxt_feedback_rectification.java
+++ src/com/pphgzs/domain/DO/jwcpxt_feedback_rectification.java
@@ -1,83 +1,48 @@
 package com.pphgzs.domain.DO;
 
-/**
- * 
- * @author JXX
- *
- */
 public class jwcpxt_feedback_rectification {
 	private String jwcpxt_feedback_rectification_id;
-	// private String feedback_rectification_dissatisfied;
+	private String feedback_rectification_dissatisfied_feedback;
+	private String feedback_rectification_no;
+	private String feedback_rectification_title;
+	private String feedback_rectification_collect_channel;
+	private String feedback_rectification_collect_time;
+	private String feedback_rectification_client_name;
+	private String feedback_rectification_client_phone;
+	private String feedback_rectification_question_describe;
+	private String feedback_rectification_unit_name;
+	private String feedback_rectification_unit_people;
+	private String feedback_rectification_unit_people_phone;
+	private int feedback_rectification_handle_state;
+	private String feedback_rectification_date;
 	private String feedback_rectification_content;
-	private String feedback_rectification_first_review_state;
-	private String feedback_rectification_first_review_time;
-	private String feedback_rectification_first_review_content;
-	private String feedback_rectification_second_review_state;
-	private String feedback_rectification_second_review_time;
-	private String feedback_rectification_second_review_content;
+	private String feedback_rectification_sjzgbm_opinion;
+	private String feedback_rectification_cpzx_opinion;
+	private int feedback_rectification_audit_state;
 	private String feedback_rectification_gmt_create;
 	private String feedback_rectification_gmt_modified;
 
 	@Override
 	public String toString() {
-		return "jwcpxt_feedback_rectification [jwcpxt_feedback_rectification_id=" + jwcpxt_feedback_rectification_id
-				+ ", feedback_rectification_content=" + feedback_rectification_content
-				+ ", feedback_rectification_first_review_state=" + feedback_rectification_first_review_state
-				+ ", feedback_rectification_first_review_time=" + feedback_rectification_first_review_time
-				+ ", feedback_rectification_first_review_content=" + feedback_rectification_first_review_content
-				+ ", feedback_rectification_second_review_state=" + feedback_rectification_second_review_state
-				+ ", feedback_rectification_second_review_time=" + feedback_rectification_second_review_time
-				+ ", feedback_rectification_second_review_content=" + feedback_rectification_second_review_content
-				+ ", feedback_rectification_gmt_create=" + feedback_rectification_gmt_create
-				+ ", feedback_rectification_gmt_modified=" + feedback_rectification_gmt_modified + "]";
-	}
-
-	public String getFeedback_rectification_first_review_state() {
-		return feedback_rectification_first_review_state;
-	}
-
-	public void setFeedback_rectification_first_review_state(String feedback_rectification_first_review_state) {
-		this.feedback_rectification_first_review_state = feedback_rectification_first_review_state;
-	}
-
-	public String getFeedback_rectification_first_review_time() {
-		return feedback_rectification_first_review_time;
-	}
-
-	public void setFeedback_rectification_first_review_time(String feedback_rectification_first_review_time) {
-		this.feedback_rectification_first_review_time = feedback_rectification_first_review_time;
-	}
-
-	public String getFeedback_rectification_first_review_content() {
-		return feedback_rectification_first_review_content;
-	}
-
-	public void setFeedback_rectification_first_review_content(String feedback_rectification_first_review_content) {
-		this.feedback_rectification_first_review_content = feedback_rectification_first_review_content;
-	}
-
-	public String getFeedback_rectification_second_review_state() {
-		return feedback_rectification_second_review_state;
-	}
-
-	public void setFeedback_rectification_second_review_state(String feedback_rectification_second_review_state) {
-		this.feedback_rectification_second_review_state = feedback_rectification_second_review_state;
-	}
-
-	public String getFeedback_rectification_second_review_time() {
-		return feedback_rectification_second_review_time;
-	}
-
-	public void setFeedback_rectification_second_review_time(String feedback_rectification_second_review_time) {
-		this.feedback_rectification_second_review_time = feedback_rectification_second_review_time;
-	}
-
-	public String getFeedback_rectification_second_review_content() {
-		return feedback_rectification_second_review_content;
-	}
-
-	public void setFeedback_rectification_second_review_content(String feedback_rectification_second_review_content) {
-		this.feedback_rectification_second_review_content = feedback_rectification_second_review_content;
+		return "jwcpxt_feedback_rectification 【\njwcpxt_feedback_rectification_id=" + jwcpxt_feedback_rectification_id
+				+ ", \nfeedback_rectification_dissatisfied_feedback=" + feedback_rectification_dissatisfied_feedback
+				+ ", \nfeedback_rectification_no=" + feedback_rectification_no + ", \nfeedback_rectification_title="
+				+ feedback_rectification_title + ", \nfeedback_rectification_collect_channel="
+				+ feedback_rectification_collect_channel + ", \nfeedback_rectification_collect_time="
+				+ feedback_rectification_collect_time + ", \nfeedback_rectification_client_name="
+				+ feedback_rectification_client_name + ", \nfeedback_rectification_client_phone="
+				+ feedback_rectification_client_phone + ", \nfeedback_rectification_question_describe="
+				+ feedback_rectification_question_describe + ", \nfeedback_rectification_unit_name="
+				+ feedback_rectification_unit_name + ", \nfeedback_rectification_unit_people="
+				+ feedback_rectification_unit_people + ", \nfeedback_rectification_unit_people_phone="
+				+ feedback_rectification_unit_people_phone + ", \nfeedback_rectification_handle_state="
+				+ feedback_rectification_handle_state + ", \nfeedback_rectification_date=" + feedback_rectification_date
+				+ ", \nfeedback_rectification_content=" + feedback_rectification_content
+				+ ", \nfeedback_rectification_sjzgbm_opinion=" + feedback_rectification_sjzgbm_opinion
+				+ ", \nfeedback_rectification_cpzx_opinion=" + feedback_rectification_cpzx_opinion
+				+ ", \nfeedback_rectification_audit_state=" + feedback_rectification_audit_state
+				+ ", \nfeedback_rectification_gmt_create=" + feedback_rectification_gmt_create
+				+ ", \nfeedback_rectification_gmt_modified=" + feedback_rectification_gmt_modified + "\n】";
 	}
 
 	public String getJwcpxt_feedback_rectification_id() {
@@ -88,12 +53,140 @@ public class jwcpxt_feedback_rectification {
 		this.jwcpxt_feedback_rectification_id = jwcpxt_feedback_rectification_id;
 	}
 
+	public String getFeedback_rectification_dissatisfied_feedback() {
+		return feedback_rectification_dissatisfied_feedback;
+	}
+
+	public void setFeedback_rectification_dissatisfied_feedback(String feedback_rectification_dissatisfied_feedback) {
+		this.feedback_rectification_dissatisfied_feedback = feedback_rectification_dissatisfied_feedback;
+	}
+
+	public String getFeedback_rectification_no() {
+		return feedback_rectification_no;
+	}
+
+	public void setFeedback_rectification_no(String feedback_rectification_no) {
+		this.feedback_rectification_no = feedback_rectification_no;
+	}
+
+	public String getFeedback_rectification_title() {
+		return feedback_rectification_title;
+	}
+
+	public void setFeedback_rectification_title(String feedback_rectification_title) {
+		this.feedback_rectification_title = feedback_rectification_title;
+	}
+
+	public String getFeedback_rectification_collect_channel() {
+		return feedback_rectification_collect_channel;
+	}
+
+	public void setFeedback_rectification_collect_channel(String feedback_rectification_collect_channel) {
+		this.feedback_rectification_collect_channel = feedback_rectification_collect_channel;
+	}
+
+	public String getFeedback_rectification_collect_time() {
+		return feedback_rectification_collect_time;
+	}
+
+	public void setFeedback_rectification_collect_time(String feedback_rectification_collect_time) {
+		this.feedback_rectification_collect_time = feedback_rectification_collect_time;
+	}
+
+	public String getFeedback_rectification_client_name() {
+		return feedback_rectification_client_name;
+	}
+
+	public void setFeedback_rectification_client_name(String feedback_rectification_client_name) {
+		this.feedback_rectification_client_name = feedback_rectification_client_name;
+	}
+
+	public String getFeedback_rectification_client_phone() {
+		return feedback_rectification_client_phone;
+	}
+
+	public void setFeedback_rectification_client_phone(String feedback_rectification_client_phone) {
+		this.feedback_rectification_client_phone = feedback_rectification_client_phone;
+	}
+
+	public String getFeedback_rectification_question_describe() {
+		return feedback_rectification_question_describe;
+	}
+
+	public void setFeedback_rectification_question_describe(String feedback_rectification_question_describe) {
+		this.feedback_rectification_question_describe = feedback_rectification_question_describe;
+	}
+
+	public String getFeedback_rectification_unit_name() {
+		return feedback_rectification_unit_name;
+	}
+
+	public void setFeedback_rectification_unit_name(String feedback_rectification_unit_name) {
+		this.feedback_rectification_unit_name = feedback_rectification_unit_name;
+	}
+
+	public String getFeedback_rectification_unit_people() {
+		return feedback_rectification_unit_people;
+	}
+
+	public void setFeedback_rectification_unit_people(String feedback_rectification_unit_people) {
+		this.feedback_rectification_unit_people = feedback_rectification_unit_people;
+	}
+
+	public String getFeedback_rectification_unit_people_phone() {
+		return feedback_rectification_unit_people_phone;
+	}
+
+	public void setFeedback_rectification_unit_people_phone(String feedback_rectification_unit_people_phone) {
+		this.feedback_rectification_unit_people_phone = feedback_rectification_unit_people_phone;
+	}
+
+	public int getFeedback_rectification_handle_state() {
+		return feedback_rectification_handle_state;
+	}
+
+	public void setFeedback_rectification_handle_state(int feedback_rectification_handle_state) {
+		this.feedback_rectification_handle_state = feedback_rectification_handle_state;
+	}
+
+	public int getFeedback_rectification_audit_state() {
+		return feedback_rectification_audit_state;
+	}
+
+	public void setFeedback_rectification_audit_state(int feedback_rectification_audit_state) {
+		this.feedback_rectification_audit_state = feedback_rectification_audit_state;
+	}
+
+	public String getFeedback_rectification_date() {
+		return feedback_rectification_date;
+	}
+
+	public void setFeedback_rectification_date(String feedback_rectification_date) {
+		this.feedback_rectification_date = feedback_rectification_date;
+	}
+
 	public String getFeedback_rectification_content() {
 		return feedback_rectification_content;
 	}
 
 	public void setFeedback_rectification_content(String feedback_rectification_content) {
 		this.feedback_rectification_content = feedback_rectification_content;
+	}
+
+	public String getFeedback_rectification_sjzgbm_opinion() {
+		return feedback_rectification_sjzgbm_opinion;
+	}
+
+	public void setFeedback_rectification_sjzgbm_opinion(String feedback_rectification_sjzgbm_opinion) {
+		this.feedback_rectification_sjzgbm_opinion = feedback_rectification_sjzgbm_opinion;
+	}
+
+	public String getFeedback_rectification_cpzx_opinion() {
+		return feedback_rectification_cpzx_opinion;
+	}
+
+	public void setFeedback_rectification_cpzx_opinion(String feedback_rectification_cpzx_opinion) {
+		this.feedback_rectification_cpzx_opinion = feedback_rectification_cpzx_opinion;
 	}
 
 	public String getFeedback_rectification_gmt_create() {
