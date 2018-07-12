c31223080855846fdc007c5f30d3476a4f07383f
sunyi970505 <1059747381@qq.com>
2018-07-12 11:08:48 +0800

    业务定义模块写完

--- WebContent/js/managerService/managerService.js
+++ WebContent/js/managerService/managerService.js
@@ -8,8 +8,9 @@ function intoQuestion(event) {
 }
 
 var interfaceVue;
-
+var definitionId;
 function showInterface(event) {
+	definitionId = event.id;
 	$
 			.confirm({
 				title : '接口管理',
@@ -19,11 +20,21 @@ function showInterface(event) {
 				content : '<button id="'
 						+ event.id
 						+ '" onclick="addInterface(this)" class="btn btn-default"><i class="ti-plus"></i>增加接口</button><table id="showInterface" class="table table-striped" style="text-align: center;"><thead><tr>'
-						+ '<td>服务编号</td>' + '<td>接口唯一标识</td>'
-						+ '<td>资源用户名</td>' + '<td>机器ip</td>' + '<td>操作</td>'
-						+ '<td></td>' + '</tr></thead>'
-						+ '<tbody><template v-for="">' + '<tr></tr>'
-						+ '</template></tbody>' + '</table>',
+						+ '<td>服务编号</td>'
+						+ '<td>接口唯一标识</td>'
+						+ '<td>资源用户名</td>'
+						+ '<td>机器ip</td>'
+						+ '<td>操作</td>'
+						+ '<td></td>'
+						+ '</tr></thead>'
+						+ '<tbody><template v-for="interface in interfaceList">'
+						+ '<tr>'
+						+ '<td>{{ interface.service_grab_service_num }}</td>'
+						+ '<td>{{ interface.service_grab_interface_identifying }}</td>'
+						+ '<td>{{ interface.service_grab_source_username }}</td>'
+						+ '<td>{{ interface.service_grab_machine_ip }}</td>'
+						+ '<td><a :id="interface.jwcpxt_service_grab_id" onclick="updateInterface(this)"><i class="ti-pencil-alt"></i></a>|<a :id="interface.jwcpxt_service_grab_id" onclick="deleteInterface(this)"><i class="ti-trash"></i></a></td>'
+						+ '</tr>' + '</template></tbody>' + '</table>',
 				buttons : {
 					cancel : {
 						text : '关闭',
@@ -47,13 +58,15 @@ function showInterface(event) {
 }
 
 function loadDataInterface(id) {
-	$.ajax({
-		url:'/jwcpxt/Service/list_serviceGrab_byServiceDefinitionId?serviceDefinition.jwcpxt_service_definition_id='+id,
-		type:'GET',
-		success:function(data){
-			interfaceVue.interfaceList = JSON.parse(data);
-		}
-	})
+	$
+			.ajax({
+				url : '/jwcpxt/Service/list_serviceGrab_byServiceDefinitionId?serviceDefinition.jwcpxt_service_definition_id='
+						+ id,
+				type : 'GET',
+				success : function(data) {
+					interfaceVue.interfaceList = JSON.parse(data);
+				}
+			})
 }
 
 function addInterface(event) {
@@ -101,7 +114,10 @@ function addInterface(event) {
 						action : function() {
 							var formData = new FormData(document
 									.getElementById("addInterface"));
-							formData.append('serviceGrab.service_grab_service_definition',event.id);
+							formData
+									.append(
+											'serviceGrab.service_grab_service_definition',
+											event.id);
 							$.ajax({
 								url : '/jwcpxt/Service/save_serviceGrab',
 								type : 'POST',
@@ -117,6 +133,142 @@ function addInterface(event) {
 									}
 								}
 							})
+
+						}
+					}
+				}
+			})
+}
+
+function updateInterface(event) {
+	$
+			.confirm({
+				title : '修改接口',
+				type : 'orange',
+				boxWidth : '900px',
+				useBootstrap : false,
+				content : '<form id="updateInterface"><table style="margin:0 auto;" class="table table-bordered">'
+						+ '<tr><td>服务编号</td><td><input type="text" name="serviceGrab.service_grab_service_num" class="form-control updateIn"></td>'
+						+ '<td>接口唯一标识</td><td><input type="text" name="serviceGrab.service_grab_interface_identifying" class="form-control updateIn"></td>'
+						+ '<td>使用机器ip</td><td><input type="text" name="serviceGrab.service_grab_machine_ip" class="form-control updateIn"></td></tr>'
+
+						+ '<tr><td>资源用户名</td><td><input type="text" name="serviceGrab.service_grab_source_username" class="form-control updateIn"></td>'
+						+ '<td>资源密码</td><td><input type="text" name="serviceGrab.service_grab_source_password" class="form-control updateIn"></td>'
+						+ '<td>业务项目名</td><td><input type="text" name="serviceGrab.service_grab_project_name" class="form-control updateIn"></td></tr>'
+
+						+ '<tr><td>是否单表</td>'
+						+ '<td><select class="form-control updateIn" name="serviceGrab.service_grab_single_table"><option value="1">是</option><option value="2">否</option></select></td>'
+						+ '<td>接口名一</td><td><input type="text" name="serviceGrab.service_grab_interface_one" class="form-control updateIn"></td>'
+						+ '<td>接口名二</td><td><input type="text" name="serviceGrab.service_grab_interface_two" class="form-control updateIn"></td></tr>'
+
+						+ '<tr><td>业务唯一识别编号</td><td><input type="text" name="serviceGrab.service_grab_field_name" class="form-control updateIn"></td>'
+						+ '<td>当事人姓名</td><td><input type="text" name="serviceGrab.service_grab_name_field" class="form-control updateIn"></td>'
+						+ '<td>当事人性别</td><td><input type="text" name="serviceGrab.service_grab_sex_field" class="form-control updateIn"></td></tr>'
+
+						+ '<tr><td>当事人电话</td><td><input type="text" name="serviceGrab.service_grab_phone_field" class="form-control updateIn"></td>'
+						+ '<td>业务办理时间</td><td><input type="text" name="serviceGrab.service_grab_handle_time_gield" class="form-control updateIn"></td>'
+						+ '<td>连接1</td><td><input type="text" name="serviceGrab.service_grab_connect_one_field" class="form-control updateIn"></td></tr>'
+
+						+ '<td>连接2</td><td><input type="text" name="serviceGrab.service_grab_connect_two_field" class="form-control updateIn"></td></tr>'
+						+ '</table></form>',
+				buttons : {
+					cancel : {
+						text : '关闭',
+						btnClass : 'btn-red',
+						action : function() {
+
+						}
+					},
+					save : {
+						text : '修改',
+						btnClass : 'btn-blue',
+						action : function() {
+							var formData = new FormData(document
+									.getElementById("updateInterface"));
+							formData
+									.append(
+											'serviceGrab.jwcpxt_service_grab_id',
+											event.id);
+							$.ajax({
+								url : '/jwcpxt/Service/update_serviceGrab_byServiceGrabId',
+								type : 'POST',
+								data : formData,
+								processData : false,
+								contentType : false,
+								success : function(data) {
+									if (data == 1) {
+										toastr.success("保存成功");
+										loadDataInterface(definitionId);
+									} else {
+										toastr.error("保存失败");
+									}
+								}
+							})
+
+						}
+					}
+				},
+				onContentReady : function() {
+					$
+							.ajax({
+								url : '/jwcpxt/Service/get_serviceGrab?serviceGrab.jwcpxt_service_grab_id='
+										+ event.id,
+								type : 'GET',
+								success : function(data) {
+									var interfaceOne = JSON.parse(data);
+									var inputUpdate = document
+											.getElementsByClassName("updateIn");
+									for (var i = 0; i < inputUpdate.length; i++) {
+										Object
+												.keys(interfaceOne)
+												.forEach(
+														function(key) {
+															if (key == inputUpdate[i].name
+																	.substring(12)) {
+																inputUpdate[i].value = interfaceOne[key];
+															}
+
+														});
+									}
+								}
+							})
+				}
+			})
+}
+
+function deleteInterface(event) {
+	$
+			.confirm({
+				title : '删除接口',
+				type : 'red',
+				icon : 'fa fa-warning',
+				content : '确定要删除吗？',
+				autoClose : 'cancelAction|8000',
+				buttons : {
+					deleteUser : {
+						text : '删除',
+						btnClass : 'btn-blue',
+						action : function() {
+							$
+									.ajax({
+										url : '/jwcpxt/Service/delete_serviceGrab_byServiceGrabId?serviceGrab.jwcpxt_service_grab_id='
+												+ event.id,
+										type : 'GET',
+										success : function(data) {
+											if (data == '1') {
+												toastr.success("删除成功");
+												loadDataInterface(definitionId);
+											} else {
+												toastr.error("删除失败");
+											}
+										}
+									})
+						}
+					},
+					cancelAction : {
+						text : '关闭',
+						btnClass : 'btn-red',
+						action : function() {
 
 						}
 					}
