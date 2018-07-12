a313c8d251cec7fe2242cf96b6a86590529105f4
ystartor@gmail.com <ystartor@gmail.com>
2018-07-12 09:38:24 +0800

    增加抓取表中部分字段

---
+++ src/com/pphgzs/domain/DTO/ClientInstanceDTO.java
@@ -0,0 +1,13 @@
+package com.pphgzs.domain.DTO;
+
+import com.pphgzs.domain.DO.jwcpxt_service_instance;
+
+/**
+ * 
+ * @author JXX
+ *
+ */
+public class ClientInstanceDTO {
+	private jwcpxt_service_instance serviceInstance;
+	
+}
