8d44f97e4747d956c1032611508ec16c7aabb87c
hy752277380 <33386194+hy752277380@users.noreply.github.com>
2018-07-15 13:27:28 +0800

    统计页面

--- WebContent/WEB-INF/view/statisticalData/statisticalData.jsp
+++ WebContent/WEB-INF/view/statisticalData/statisticalData.jsp
@@ -25,6 +25,8 @@ th {
 </style>
 <link rel="stylesheet"
 	href="<%=basePath%>css/statisticalData/doublebox-bootstrap.css">
+<link rel="stylesheet"
+	href="<%=basePath%>css/statisticalData/element.min.css">
 <title>统计数据</title>
 </head>
 <body>
@@ -33,7 +35,8 @@ th {
 		<s:action name="skipSidebar" namespace="/Skip" executeResult="true" />
 		<div class="main-panel">
 			<!-- 引入导航条  -->
-			<s:action name="skipNavbarIndex" namespace="/Skip" executeResult="true" />
+			<s:action name="skipNavbarIndex" namespace="/Skip"
+				executeResult="true" />
 			<div class="content" id="content">
 				<div class="container-fluid">
 					<!-- 主内容 -->
@@ -44,38 +47,101 @@ th {
 									<h4 class="title">统计数据</h4>
 								</div>
 								<div class="content table-responsive table-full-width">
-									<button type="button" class="btn btn-default"
+									<!-- <button type="button" class="btn btn-default"
 										@click="beginStatistical">
 										<i class="ti-plus"></i>开始统计
-									</button>
+									</button> -->
+
+									<div style="height: 70px; width: 100%; ">
+										<el-steps finish-status="success" :active="3"> <el-step
+											title="步骤 1" description="选择单位"></el-step> <el-step
+											title="步骤 2" description="选择业务"></el-step> <el-step
+											title="步骤 3" description="业务给分"> </el-step> </el-steps>
+									</div>
+									<div style="width: 100%; margin-top: 30px;">
+										<div style="width: 90%; margin: auto;">
+											<div class="form-group">
+												<h2>选择单位</h2>
+												<select class="statistical-selected" id="unit"
+													multiple="multiple" size="10"></select>
+											</div>
+										</div>
+										<div style="width: 90%; margin: auto; margin-top: 30px;">
+											<div class="form-group">
+												<h2>选择业务</h2>
+												<select class="statistical-selected" id="service"
+													multiple="multiple" size="10"></select>
+											</div>
+										</div>
+										<div style="width: 90%; margin: auto; margin-top: 30px;"
+											v-cloak>
+											<h2>业务分数</h2>
+											<template v-for="(service,index) in serviceSelect">
+											<div class="form-group">
+												<label>{{service.service_definition_describe}}</label> <input
+													type="text" style="width:240px;"
+													class="form-control inputScore" :index="index"
+													:serviceid="service.jwcpxt_service_definition_id"
+													placeholder="请输入业务分数..." onblur="this.v();"
+													onkeyup="(this.v=function(){this.value=this.value.replace(/[^0-9-]+/,'');}).call(this)">
+											</div>
+											</template>
+										</div>
+										<div style="width: 90%; margin: auto; margin-top: 30px;">
+											<div class="form-group" style="margin: auto;">
+												<label>起始时间</label> <input id="beginTime" placeholder="起始时间"
+													class="mydate form-control"
+													style="display: inline; width: 150px;"> <label>结束时间</label>
+												<input id="endTime" placeholder="结束时间"
+													class="mydate form-control"
+													style="display: inline; width: 150px;">
+											</div>
+										</div>
+										<div
+											style="width: 90%; margin: auto; margin-top: 30px; text-align: center;">
+											<button type="button" class="btn btn-default"
+												@click="reviewResult" style="margin: auto;">
+												<i class="ti-plus"></i>预览统计结果
+											</button>
+											<button type="button" class="btn btn-default"
+												@click="exportResult" style="margin: auto;">
+												<i class="ti-plus"></i>导出统计结果
+											</button>
+										</div>
+									</div>
+
 									<!-- 
 										1.多选单位
 										2.多选业务
-										每个业务要给分数，所有业务的分数加起来等于100分，不等就要报错
+										每个业务要给分数，所有业务的分数加起来等于100分
 									 -->
-									<table v-if="ready" class="table" style="text-align: center; width: 100%;">
+									<table v-if="ready" class="table"
+										style="text-align: center; width: 100%;">
 										<thead>
 											<tr>
 												<template v-if="statisticalResultsData.length>0">
-													<th style="width: 150px;">评测内容单位</th>
-														<template v-for="serviceGradeDTO in statisticalResultsData[0].serviceGradeBelongUnitDTOList">
-															<th>{{serviceGradeDTO.serviceDefinition.service_definition_describe}}</th>
-														</template>
-													<th style="width: 60px;">测评总分</th>
-													<th style="width: 40px;">排名</th>
+												<th style="width: 150px;">评测内容单位</th>
+												<template
+													v-for="serviceGradeDTO in statisticalResultsData[0].serviceGradeBelongUnitDTOList">
+												<th>{{serviceGradeDTO.serviceDefinition.service_definition_describe}}</th>
+												</template>
+												<th style="width: 60px;">测评总分</th>
+												<th style="width: 40px;">排名</th>
 												</template>
 											</tr>
 										</thead>
 										<tbody>
-											<template v-for="(statisticalResultsDTO,index) in statisticalResultsData">
-												<tr>
-													<td>{{statisticalResultsDTO.unit.unit_name}}</td>
-													<template v-for="serviceGradeDTO in statisticalResultsDTO.serviceGradeBelongUnitDTOList">
-														<td>{{serviceGradeDTO.grade}}</td>
-													</template>
-													<td>{{100}}</td>
-													<td>{{index+1}}</td>
-												</tr>
+											<template
+												v-for="(statisticalResultsDTO,index) in statisticalResultsData">
+											<tr>
+												<td>{{statisticalResultsDTO.unit.unit_name}}</td>
+												<template
+													v-for="serviceGradeDTO in statisticalResultsDTO.serviceGradeBelongUnitDTOList">
+												<td>{{serviceGradeDTO.grade}}</td>
+												</template>
+												<td>{{100}}</td>
+												<td>{{index+1}}</td>
+											</tr>
 											</template>
 										</tbody>
 									</table>
@@ -89,11 +155,13 @@ th {
 			<s:action name="skipFooter" namespace="/Skip" executeResult="true" />
 		</div>
 	</div>
+	<form method="POST" action="" id="exportForm"></form>
 </body>
 <script type="text/javascript">
 	/* 处理侧边栏选项 */
 	$('#sideManager').attr("class", "active");
 </script>
+<script src="<%=basePath%>js/statisticalData/element.min.js"></script>
 <script src="<%=basePath%>js/statisticalData/doublebox-bootstrap.js"></script>
 <script src="<%=basePath%>js/statisticalData/statisticalData.js"></script>
 </html>
--- WebContent/js/statisticalData/statisticalData.js
+++ WebContent/js/statisticalData/statisticalData.js
@@ -18,9 +18,10 @@ $(function() {
 			buttonText : '下一步-打分',
 			allData : [], //所有业务信息
 		},
-		unitSelect : [],
+		unitSelect : [], //暂时未使用
 		unitSelectIdArr : [],
-		serviceSelect : []
+		serviceSelect : [],
+		serviceSelectArr : [],
 	}
 
 	let vm = new Vue({
@@ -29,20 +30,223 @@ $(function() {
 		methods : {
 			beginStatistical () {
 				selected(myData.unit);
+			},
+			reviewResult () {
+				reviewStatisticalResult();
+			},
+			exportResult () {
+				exportStatisticalResult();
 			}
 		},
 		mounted () {
 			//获取所有已有被分配业务的单位
 			$.post(myData.unit.url, {}, response => {
 				myData.unit.allData = response;
+				var demo2 = $('#unit').doublebox({
+					nonSelectedListLabel : '未选择单位',
+					selectedListLabel : '已选择单位',
+					preserveSelectionOnMove : 'moved',
+					moveOnSelect : false,
+					doubleMove : true,
+					optionValue : myData.unit.optionValue,
+					optionText : myData.unit.optionText,
+					nonSelectedList : myData.unit.allData,
+					selectedList : []
+				});
+				$('#unit').change(function() {
+					myData.unitSelectIdArr = $(this).val();
+				});
 			}, 'json');
 			//获取所有的业务
 			$.post(myData.service.url, {}, response => {
 				myData.service.allData = response;
+				var demo2 = $('#service').doublebox({
+					nonSelectedListLabel : '未选择业务',
+					selectedListLabel : '已选择业务',
+					preserveSelectionOnMove : 'moved',
+					moveOnSelect : false,
+					doubleMove : true,
+					optionValue : myData.service.optionValue,
+					optionText : myData.service.optionText,
+					nonSelectedList : myData.service.allData,
+					selectedList : []
+				});
+				$('#service').change(function() {
+					let arr = [];
+					myData.serviceSelectArr = $(this).val();
+					myData.serviceSelectArr.forEach((id, i) => {
+						$.each(myData.service.allData, function(index, ele) {
+							if (ele.jwcpxt_service_definition_id == id) {
+								arr.push(ele);
+								return true;
+							}
+						})
+					})
+					myData.serviceSelect = arr;
+				});
 			}, 'json');
 		},
 	})
 
+	timeUtil();
+	function timeUtil() {
+		$.datetimepicker.setLocale('ch');
+		$('.mydate').datetimepicker({
+			pickerPosition : "top-right",
+			yearStart : 1900, // 设置最小年份
+			yearEnd : 2050, // 设置最大年份
+			yearOffset : 0, // 年偏差
+			timepicker : false, // 关闭时间选项
+			format : 'Y-m-d', // 格式化日期年-月-日
+			minDate : '1900/01/01', // 设置最小日期
+			maxDate : '2050/01/01', // 设置最大日期
+		});
+	}
+
+
+	//预览统计结果
+	function reviewStatisticalResult() {
+		let validation = statisticalValidation();
+		if (validation) {
+			let reviewStatisticalResultConfirm = $.confirm({
+				smoothContent : false, //关闭动画
+				closeIcon : true, //关闭图标
+				closeIconClass : 'fa fa-close', //图标样式
+				type : 'dark', //弹出框类型
+				boxWidth : '80%', //设置宽度
+				useBootstrap : false, //设置是否使用bootstropt样式
+				title : '统计结果',
+				content : `
+				<div id="reviewStatisticalResultConfirm" style="width:100%;">
+					<table v-if="ready" class="table" style="text-align: center; width: 100%;">
+						<thead>
+							<tr>
+								<template v-if="statisticalResultsData.length>0">
+								<th style="width: 150px;">评测内容单位</th>
+									<template v-for="serviceGradeDTO in statisticalResultsData[0].serviceGradeBelongUnitDTOList">
+										<th>{{serviceGradeDTO.serviceDefinition.service_definition_describe}}</th>
+									</template>
+								<th style="width: 60px;">测评总分</th>
+								<th style="width: 40px;">排名</th>
+								</template>
+							</tr>
+						</thead>
+						<tbody>
+							<template v-for="(statisticalResultsDTO,index) in statisticalResultsData">
+							<tr>
+								<td>{{statisticalResultsDTO.unit.unit_name}}</td>
+								<template v-for="serviceGradeDTO in statisticalResultsDTO.serviceGradeBelongUnitDTOList">
+								<td>{{serviceGradeDTO.grade}}</td>
+								</template>
+								<td>{{100}}</td>
+								<td>{{index+1}}</td>
+							</tr>
+							</template>
+						</tbody>
+					</table>
+				</div>
+				`,
+				onContentReady : function() {
+					let reviewStatisticalResultConfirmVue = new Vue({
+						el : '#reviewStatisticalResultConfirm',
+						data : {
+							statisticalResultsData : [],
+							ready : false
+						},
+						mounted () {
+							let formData = new FormData();
+							formData.append('searchTimeStart', $('#beginTime').val()); //筛选起始时间
+							formData.append('searchTimeEnd', $('#endTime').val()); //筛选结束时间
+							validation.forEach(function(element, i) {
+								formData.append('serviceGradeDTOList[' + i + '].service_id', element.serviceid);
+								formData.append('serviceGradeDTOList[' + i + '].grade', element.score);
+							})
+							myData.unitSelectIdArr.forEach(function(element, i) {
+								formData.append('unitIds', element);
+							})
+							$.ajax({
+								url : '/jwcpxt/Statistics/getGradeByCondition',
+								type : 'POST',
+								dataType : 'json',
+								cache : false,
+								data : formData,
+								processData : false,
+								contentType : false,
+								success : response => {
+									this.statisticalResultsData = response.unitHaveServiceGradeDTOList;
+									this.ready = true;
+								}
+							});
+
+						}
+					});
+				},
+				buttons : {
+					confirm : {
+						text : '导出',
+						btnClass : 'btn-blue',
+						action : function() {}
+					},
+					cancel : {
+						text : '取消',
+						btnClass : 'btn-default',
+						keys : [ 'esc' ],
+						action : function() {}
+					}
+				},
+			});
+		}
+	}
+	function exportStatisticalResult() {
+		let validation = statisticalValidation();
+		if (validation) {
+			let form = $('#exportForm');
+			//先清空表单
+			form.empty();
+			form.attr('action', '/jwcpxt/Statistics/exportStatisticsExcel');
+			validation.forEach(function(element, i) {
+				form.append($(`<input type="hidden" name="${'serviceGradeDTOList[' + i + '].service_id'}" value="${element.serviceid}">`));
+				form.append($(`<input type="hidden" name="${'serviceGradeDTOList[' + i + '].grade'}" value="${element.score}">`));
+			})
+			myData.unitSelectIdArr.forEach(function(element, i) {
+				form.append($(`<input type="hidden" name="unitIds" value="${element}">`));
+			})
+			form.submit(); //自动提交
+		}
+	}
+
+	function statisticalValidation() {
+		if ($('#unit').val().length < 1) {
+			toastr.error('单位不能为空');
+			return false;
+		}
+		if ($('#service').val().length < 1) {
+			toastr.error('业务不能为空');
+			return false;
+		}
+		let scoreData = [];
+		let num = 0;
+		$('.inputScore').each((index, element) => {
+			let score_serviceid = {
+				serviceid : $(element).attr('serviceid'),
+				score : $(element).val(),
+			};
+			num += Number(score_serviceid.score);
+			scoreData[index] = score_serviceid;
+		});
+		if (num < 100) {
+			toastr.error('总分不足100，请检查');
+			return false;
+		} else if (num > 100) {
+			toastr.error('总分超过100，请检查');
+			return false;
+		}
+		if ($('#beginTime').val() > $('#endTime').val()) {
+			toastr.error('时间区间中结束时间不能小于开始时间，请检查');
+			return false;
+		}
+		return scoreData;
+	}
 
 	function selected(option) {
 		//--------------------
