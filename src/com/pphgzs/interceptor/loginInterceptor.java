41676775677da9565919f67621f4b7096732871b
ystartor@gmail.com <ystartor@gmail.com>
2018-07-22 14:40:44 +0800

    测试文档

--- src/com/pphgzs/interceptor/loginInterceptor.java
+++ src/com/pphgzs/interceptor/loginInterceptor.java
@@ -9,7 +9,6 @@ public class loginInterceptor extends AbstractInterceptor {
 	public String intercept(ActionInvocation invocation) throws Exception {
 		// 得到拦截到的action的名称,看是否是login,当是login的时候,不用进行下面的检测了,直接执行下一个拦截器
 		String actionName = invocation.getProxy().getActionName();
-		System.out.println("actionName:" + actionName);
 		if ("skipSystemIndex".equals(actionName) || "skipSidebar".equals(actionName)
 				|| "skipNavbarIndex".equals(actionName) || "skipFooter".equals(actionName)) {
 			return invocation.invoke();
@@ -17,7 +16,6 @@ public class loginInterceptor extends AbstractInterceptor {
 		// 如果不是login.则判断是否已登录,及检测session中key为user的值是否存在,如果不存在,跳回到登录页面
 		String loginType = (String) invocation.getInvocationContext().getSession().get("loginType");
 		if (loginType == null) {
-			System.out.println("未登录");
 			return "login";
 		}
 		// 进行到这里.说明用户已登录,则跳转到下一个拦截器
